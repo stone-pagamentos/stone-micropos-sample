16253edb1dc99b14bcb920872c0639c5a8e91e46
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-03-16 18:51:05 -0300

    :memo: TransactionOption doc.

--- SimpleConsoleApp/CommandLine/Options/TransactionOption.cs
+++ SimpleConsoleApp/CommandLine/Options/TransactionOption.cs
@@ -3,14 +3,34 @@ using Pinpad.Sdk.Model;
 
 namespace SimpleConsoleApp.CmdLine.Options
 {
-    // TODO: Doc
+    /// <summary>
+    /// Responsible for store transaction data.
+    /// </summary>
+    /// <example>
+    ///     --valor 20.66 -id myTransaction567
+    ///     --valor 20.66 -id myTransaction567 -tipo debit
+    ///     --valor 20.66 -id myTransaction567 -tipo credit
+    /// </example>
     internal sealed class TransactionOption
     {
+        /// <summary>
+        /// Transaction amount.
+        /// </summary>
         [Option("valor", Required = true)]
         public decimal Amount { get; set; }
-        [Option("tipo", Required = false)]
-        public TransactionType TransactionType { get; set; }
+        /// <summary>
+        /// Initiator Transaction Key. Really important to fill this
+        /// property with a primary key for that transaction.
+        /// If it's not used, our SDK will provide a GUID for it.
+        /// </summary>
         [Option("id", Required = true)]
         public string Itk { get; set; }
+        /// <summary>
+        /// Transaction type. If undefined, the pinpad will prompt
+        /// for the cardholder to choose between all possible supported
+        /// applications for that card.
+        /// </summary>
+        [Option("tipo", Required = false)]
+        public TransactionType TransactionType { get; set; }
     }
 }
