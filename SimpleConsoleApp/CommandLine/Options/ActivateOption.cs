b0ba7d4fef9d98828762983a6c4455b5ace549ba
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-03-16 18:14:37 -0300

    :memo: ActivateOption doc.

--- SimpleConsoleApp/CommandLine/Options/ActivateOption.cs
+++ SimpleConsoleApp/CommandLine/Options/ActivateOption.cs
@@ -2,11 +2,24 @@
 
 namespace SimpleConsoleApp.CmdLine.Options
 {
-    // TODO: Doc
+    /// <summary>
+    /// Responsible for store data to connect to one pinpad.
+    /// </summary>
+    /// <example>
+    ///     --stoneCode 12345678 --port 1234
+    ///     --stoneCode 
+    ///     --stonecode 12345678
+    /// </example>
     internal sealed class ActivateOption
     {
+        /// <summary>
+        /// Merchant StoneCode.
+        /// </summary>
         [Option("stoneCode", Required = true)]
         public string StoneCode { get; set; }
+        /// <summary>
+        /// Port of the computer in which the pinpad is attached
+        /// </summary>
         [Option("porta", DefaultValue = null, Required = false)]
         public string Port  { get; set; }
     }
