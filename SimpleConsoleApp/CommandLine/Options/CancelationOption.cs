14e68f5d88f5fb4b708527f576896da28a04e21a
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-03-16 18:17:31 -0300

    :memo: CancelationOption doc.

--- SimpleConsoleApp/CommandLine/Options/CancelationOption.cs
+++ SimpleConsoleApp/CommandLine/Options/CancelationOption.cs
@@ -2,10 +2,25 @@
 
 namespace SimpleConsoleApp.CmdLine.Options
 {
+    /// <summary>
+    /// Responsible for store data to cancel a transaction by it's
+    /// Stone ID (Acquirer Transaction Key, ATK). 
+    /// </summary>
+    /// <example>
+    ///     --stoneId 1234567890123 --amount 15.99
+    ///     --stoneid 1234567890123 --amount 15
+    /// </example>
     internal sealed class CancelationOption
     {
+        /// <summary>
+        /// Transaction ID set by Stone.
+        /// </summary>
         [Option("stoneId", Required = true)]
         public string StoneId { get; set; }
+        /// <summary>
+        /// Transaction amount. Should match the original transaction,
+        /// otherwise it will cancel the transaction partially.
+        /// </summary>
         [Option("valor", Required = true)]
         public decimal Amount { get; set; }
     }
