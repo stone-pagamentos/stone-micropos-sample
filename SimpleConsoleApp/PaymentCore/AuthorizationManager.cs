aaaac5b3c49e7ee7b9c7ba5fc6f1dca7cdd50448
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-03-16 19:09:45 -0300

    :memo: AuthorizationManager doc.

--- SimpleConsoleApp/PaymentCore/AuthorizationManager.cs
+++ SimpleConsoleApp/PaymentCore/AuthorizationManager.cs
@@ -3,14 +3,24 @@ using SimpleConsoleApp.CmdLine;
 
 namespace SimpleConsoleApp.PaymentCore
 {
-    // TODO: Doc.
+    /// <summary>
+    /// Reads the command line arguments and process the corresponding
+    /// action.
+    /// </summary>
     internal sealed class AuthorizationManager
     {
+        /// <summary>
+        /// Initializes the desktop information. If this initialization is missing, 
+        /// <see cref="MicroPos"/> won't act correctly.
+        /// </summary>
         public AuthorizationManager()
         {
             MicroPos.Platform.Desktop.DesktopInitializer.Initialize();
         }
 
+        /// <summary>
+        /// Reads the next command until an "sair" command is received.
+        /// </summary>
         public void Run ()
         {
             bool getOutOfHere = false;
@@ -23,6 +33,10 @@ namespace SimpleConsoleApp.PaymentCore
             while (getOutOfHere == false);
         }
 
+        /// <summary>
+        /// Reads a new command.
+        /// </summary>
+        /// <returns>The command read.</returns>
         private string ReadNextCommand ()
         {
             Console.Write("> ");
